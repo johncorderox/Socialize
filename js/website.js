820494ce776c9f8f72383a903dc0126dcdf40f30
John Cordero <bbbitemarksss@gmail.com>
2016-07-03 21:04:28 -0700

    more changes!

--- js/website.js
+++ js/website.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
 
   });
 
-
 });
 
 function showContent() {
@@ -23,23 +22,3 @@ function showContent() {
   $('.code').show();
 
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-if (userURL.value == "") {
-  alert("You did not enter a URL");
-}
-else if (userURL.value.length < 8) {
-  alert("Not a valid URL, Please try again.");
-}
-var placeholder = userURL.value;
-*/
